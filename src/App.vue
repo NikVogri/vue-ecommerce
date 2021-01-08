50b80ed9461fd2e25ac8e836f0076ac16c2116c5
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-01-08 22:13:03 +0100

    feat: app logic

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,103 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <navigation />
+    <main class="container">
+      <router-view />
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navigation from "./components/partials/Navigation.vue";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    Navigation
+  },
+  mounted() {
+    this.$store.dispatch("persistUser");
+    this.$store.watch(
+      (state, getters) => getters.productsInCart,
+      newVal => this.updateCart(newVal)
+    );
+
+    this.$store.dispatch("persistCart");
+  },
+  methods: {
+    updateCart(newVal) {
+      localStorage.setItem("cart", JSON.stringify(newVal));
+    }
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap");
+
+/* ./src/index.css */
+
+/*! @import */
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: roboto, sans-serif;
+}
+
+.container {
+  width: 1024px;
+  margin: 0 auto;
+}
+nav {
+  width: 100%;
+  height: 50px;
+  @apply border-b;
+}
+
+.navigation {
+  &__inner {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 100%;
+    max-width: 1024px;
+    width: 90%;
+    margin: 0 auto;
+
+    a {
+      text-decoration: none;
+    }
+  }
+
+  &__links {
+    display: flex;
+    list-style: none;
+
+    & li {
+      &:not(:last-child) {
+        margin-right: 15px;
+      }
+    }
+  }
+}
+
+footer {
+  position: absolute;
+  bottom: 0;
+  left: 0;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 50px;
+  line-height: 50px;
+  @apply border-t;
 }
 </style>
